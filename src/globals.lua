c357fd173ab7224e949007d3203c51deccb8b2b7
Diminim <diminiminim@gmail.com>
2023-02-10 18:59:12 -0700

    set up cimgui

---
+++ src/globals.lua
@@ -0,0 +1,4 @@
+local lib_path = '/home/diminiminim/GameDev/VSLike/src' .. '/lib/cimgui'
+local extension = jit.os == 'Windows' and 'dll' or jit.os == 'Linux' and 'so' or jit.os == 'OSX' and 'dylib'
+package.cpath = string.format('%s;%s/?.%s', package.cpath, lib_path, extension)
+imgui = require('lib/cimgui/cimgui')
--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,72 @@
-function love.update()
+require("globals")
 
+love.load = function ()
+    imgui.love.Init()
 end
 
-function love.draw()
+love.draw = function ()
+    -- example window
+    imgui.ShowDemoWindow()
+    
+    -- code to render imgui
+    imgui.Render()
+    imgui.love.RenderDrawLists()
+end
 
+love.update = function (dt)
+    imgui.love.Update(dt)
+    imgui.NewFrame()
+end
+
+love.mousemoved = function (x, y, ...)
+    imgui.love.MouseMoved(x, y)
+    if not imgui.love.GetWantCaptureMouse() then
+        -- your code here
+    end
+end
+
+love.mousepressed = function (x, y, button, ...)
+    imgui.love.MousePressed(button)
+    if not imgui.love.GetWantCaptureMouse() then
+        -- your code here 
+    end
+end
+
+love.mousereleased = function (x, y, button, ...)
+    imgui.love.MouseReleased(button)
+    if not imgui.love.GetWantCaptureMouse() then
+        -- your code here 
+    end
+end
+
+love.wheelmoved = function (x, y)
+    imgui.love.WheelMoved(x, y)
+    if not imgui.love.GetWantCaptureMouse() then
+        -- your code here 
+    end
+end
+
+love.keypressed = function (key, ...)
+    imgui.love.KeyPressed(key)
+    if not imgui.love.GetWantCaptureKeyboard() then
+        -- your code here 
+    end
+end
+
+love.keyreleased = function (key, ...)
+    imgui.love.KeyReleased(key)
+    if not imgui.love.GetWantCaptureKeyboard() then
+        -- your code here 
+    end
+end
+
+love.textinput = function (t)
+    imgui.love.TextInput(t)
+    if imgui.love.GetWantCaptureKeyboard() then
+        -- your code here 
+    end
+end
+
+love.quit = function ()
+    return imgui.love.Shutdown()
 end
