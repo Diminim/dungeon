0be67ddc6ae5e18bed44dcf07199e487015a4a5b
Diminim <diminiminim@gmail.com>
2023-02-10 20:03:27 -0700

    .

--- src/main.lua
+++ src/main.lua
@@ -1,21 +1,40 @@
-require("globals")
+require('globals')
+
+local character = {
+    info = {
+        name = 'A',
+        str = 10,
+        def = 10,
+        spd = 10
+    }
+}
+
+local imgui_window = function (self)
+    if imgui.Begin('Menu') then
+        imgui.Text('Name: '..self.name)
+        imgui.Text('Str: '..self.str)
+        imgui.Text('Def: '..self.def)
+        imgui.Text('Spd: '..self.spd)
+    end
+    imgui.End()
+end
 
 love.load = function ()
     imgui.love.Init()
 end
 
-love.draw = function ()
-    -- example window
-    imgui.ShowDemoWindow()
-    
-    -- code to render imgui
-    imgui.Render()
-    imgui.love.RenderDrawLists()
-end
-
 love.update = function (dt)
     imgui.love.Update(dt)
     imgui.NewFrame()
+end
+
+love.draw = function ()
+    imgui.ShowDemoWindow()
+
+    imgui_window(character.info)
+    
+    imgui.Render()
+    imgui.love.RenderDrawLists()
 end
 
 love.mousemoved = function (x, y, ...)
