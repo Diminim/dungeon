6526edae64b3b0d9fb546ee42f35001255194d45
Diminim <diminiminim@gmail.com>
2023-02-15 17:04:47 -0700

    put events in their own file

--- src/main.lua
+++ src/main.lua
@@ -127,52 +127,11 @@ end
 
 -- -------------------------------------------------------------------------- --
 local character_infos = require('character_infos')
+local events = require('events')
 
 local battle
 
-local events = {
-	character_died = function (battle)
-		for i, v in ipairs(battle.groups.all) do
-			local message
-			if (v.info.is_dead == false) and (v.info.hp <= 0) then
-				v.info.is_dead = true
-				message = ('%s %s'):format(
-					v.info.name, 
-					'died!'
-				)
-			end
-			if message then 
-				table.insert(battle.log, message) 
-			end
-		end
-	end,
 
-	allies_died = function (battle)
-		local dead_number = 0
-		for i, v in ipairs(battle.groups.allies) do
-			if (v.info.is_dead == true) then
-				dead_number = dead_number + 1
-			end
-		end
-		if dead_number == #battle.groups.allies then
-			local message = 'Allies have been defeated...'
-			table.insert(battle.log, message)
-		end
-	end,
-
-	enemies_died = function (battle)
-		local dead_number = 0
-		for i, v in ipairs(battle.groups.enemies) do
-			if (v.info.is_dead == true) then
-				dead_number = dead_number + 1
-			end
-		end
-		if dead_number == #battle.groups.enemies then
-			local message = 'Enemies have been defeated!'
-			table.insert(battle.log, message)
-		end
-	end,
-}
 
 local function gui_generate_actions (character)
 	local actions = List:new()
